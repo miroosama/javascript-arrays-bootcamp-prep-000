2e48fb802d360684811b74648581df56be819ad7
osama miro <o.miro650@gmail.com>
2017-11-17 19:00:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles" ];
-array = ["foo"]
+
 function addElementToBeginningOfArray(array, element){
 var myArray = []
 array.unshift(element)
 return myArray;
 }
-addElementToBeginningOfArray(array, 1)
+addElementToBeginningOfArray(["foo"], 1)
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   var array = []
