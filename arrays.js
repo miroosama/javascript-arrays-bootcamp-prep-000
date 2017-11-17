7bf586be7f8dd214f2a923dfa69c54511e09785b
osama miro <o.miro650@gmail.com>
2017-11-17 18:50:14 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles" ];
+
+function addElementToBeginningOfArray(array, element){
+var array = []
+array.unshift(element)
+return array;
+}
+addElementToBeginningOfArray(["foo"], 1)
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  var array = []
+}
